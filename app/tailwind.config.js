1901e302cc5fc15ef7a047680a7b7a02efa0fe78
Dan Peavey <danpeavey@gmail.com>
2021-07-18 11:26:23 -0700

    Defining a background image for bg-frontpage-image.

---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "frontpage-image": "url('~/assets/background-scroller.png')"
+      })
+    }
+  }
+}
