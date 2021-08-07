43ce86b026e01c972f767ac30c9a5d7d133ad475
Dan Peavey <danpeavey@gmail.com>
2021-08-07 12:46:19 -0500

    Cleaning up CSS to better respect hierarchy.

--- app/components/FrontPage/FrontPageHeader.vue
+++ app/components/FrontPage/FrontPageHeader.vue
@@ -1,24 +1,36 @@
 ﻿<template>
-  <div class="text-center">
-    <i class="text-white p-3 fas fa-code fa-5x" />
-    <h2 class="text-white text-3xl mb-6">{{ myName }}</h2>
-    <p class="text-base m-0 text-s2grey">{{ city }}, {{ statePostalCode }}</p>
-    <div class="text-2xl text-s2grey">
-      <a :href="twitterUrl"><i class="p-2 hover:text-white fab fa-twitter" /></a>
-      <a :href="linkedInUrl"><i class="p-2 hover:text-white fab fa-linkedin" /></a>
-      <a :href="gitHubUrl"><i class="p-2 hover:text-white fab fa-github" /></a>
+  <div class="mx-auto text-center grid grid-cols-1">
+    <site-icon class="text-white mx-auto w-20 h-20 mb-4"/>
+    <h2 class="text-white text-3xl">{{ myName }}</h2>
+    <p class="text-base text-s2grey mb-6">{{ city }}, {{ statePostalCode }}</p>
+    <div class="inline-flex gap-x-2 mx-auto mb-2 text-2xl text-s2grey">
+      <a :href="twitterUrl"><twitter-icon class="w-8 h-8"/></a>
+      <a :href="linkedInUrl"><linkedin-icon class="w-8 h-8"/></a>
+      <a :href="gitHubUrl"><github-icon class="w-8 h-8"/></a>
     </div>
-    <vvs-logo class="fill-current text-white w-24 h-24 mx-auto" />
+    <a class="w-28 h-24 mt-2 mx-auto" :href="currentJobUrl">
+      <vvs-icon class="fill-current text-white"/>
+    </a>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import VvsLogo from "~/components/FrontPage/VvsLogo.vue";
+import TwitterIcon from "~/components/IconComponents/TwitterIcon.vue";
+import LinkedinIcon from "~/components/IconComponents/LinkedinIcon.vue";
+import GithubIcon from "~/components/IconComponents/GitHubIcon.vue";
+import VvsIcon from "~/components/IconComponents/VvsIcon.vue";
+import SiteIcon from "~/components/IconComponents/SiteIcon.vue";
 
 export default Vue.extend({
   name: "FrontPageHeader",
-  components: { VvsLogo },
+  components: {
+    SiteIcon,
+    TwitterIcon,
+    LinkedinIcon,
+    GithubIcon,
+    VvsIcon
+  },
   props: {
     myName: {
       type: String,
@@ -44,12 +56,10 @@ export default Vue.extend({
       type: String,
       default: "DEFAULT_GITHUB"
     },
+    currentJobUrl: {
+      type: String,
+      default: "DEFAULT_CURRENT_JOB"
+    }
   }
 })
 </script>
-
-<style scoped>
-.mx-0auto {
-  margin: 0 auto;
-}
-</style>
