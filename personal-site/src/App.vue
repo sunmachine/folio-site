3ce70849cc3b81dbf3de50c0d4808975ba7082d3
Dan Peavey <danpeavey@gmail.com>
2022-02-20 21:11:29 -0800

    Implementing updated background container. Updating tailwind config, z-positions, and using arbitrary bg style.

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-import FrontPage from './components/FrontPage/FrontPage.vue';
+import BackgroundContainer from "./components/FrontPage/BackgroundContainer.vue";
+
 </script>
 
 <template>
-  <FrontPage />
+  <div>
+    <BackgroundContainer />
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
