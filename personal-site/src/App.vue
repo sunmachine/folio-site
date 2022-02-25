dbcf3f0499392b259e77aa9210268d607008314b
Dan Peavey <danpeavey@gmail.com>
2022-02-24 19:48:15 -0800

    Implementing SVG icons and classdojo icon.

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import BackgroundContainer from "./components/FrontPage/BackgroundContainer.vue";
+import HeaderContainer from "./components/FrontPage/HeaderContainer.vue";
 
 </script>
 
 <template>
   <div>
     <BackgroundContainer />
+    <HeaderContainer />
   </div>
 </template>
