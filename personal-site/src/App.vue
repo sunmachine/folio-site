b38d842b7da22cb63a4a981469de3f702dccc157
Dan Peavey <danpeavey@gmail.com>
2022-02-25 18:33:50 -0800

    Screen split.

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import BackgroundContainer from "./components/FrontPage/BackgroundContainer.vue";
 import HeaderContainer from "./components/FrontPage/HeaderContainer.vue";
 
 </script>
 
 <template>
   <div>
-    <!-- TODO: I broke this parallaxing somehow. -->
-    <BackgroundContainer />
-    <div class="md:flex h-screen grid grid-cols-1 p-2">
-      <div class="md:flex-initial mt-12 mx-auto py-6">
-        <HeaderContainer />
+    <div
+      class="left-0 w-2/5 h-screen bg-top fixed bg-slate-700 border-l-8 border-t-8 border-b-8 border-gray-800"
+    >
+      <div class="md:flex h-screen grid grid-cols-1">
+        <div class="md:flex-initial mt-12 mx-auto py-6">
+          <HeaderContainer />
+        </div>
       </div>
-      <div class="md:flex-initial md:w-3/5 md:pt-16 w-screen pt-4 pl-6">
+    </div>
+    <div
+      class="right-0 p-1/2 w-3/5 h-screen absolute bg-cover bg-top bg-no-repeat bg-[url('/src/assets/background-scroller.jpg')] bg-slate-800 border-r-8 border-t-8 border-b-8 border-slate-800"
+    >
+      <div class="md:flex-initial w-max md:w-3/5 md:pt-16 pt-4 pl-6">
         <p class="text-slate-300">New site updates coming soon. I'm serious.</p>
       </div>
     </div>
