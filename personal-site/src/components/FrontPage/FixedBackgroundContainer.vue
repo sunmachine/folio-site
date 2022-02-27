23d63f61e972cdf2454b768b44fe8699170ea475
Dan Peavey <danpeavey@gmail.com>
2022-02-26 18:45:07 -0800

    Background setup.

--- personal-site/src/components/FrontPage/FixedBackgroundContainer.vue
+++ personal-site/src/components/FrontPage/FixedBackgroundContainer.vue
@@ -1,37 +1,14 @@
-<script setup lang="ts">
-import { ref } from 'vue'
-
-const movetarget = ref(null);
-
-function onMove(evt: MouseEvent) {
-    const amplitude: number = 25;
-    const element = <HTMLElement><unknown>movetarget.value;
-
-    const width = amplitude / window.innerWidth;
-    const height = amplitude / window.innerHeight;
-    const pageX = evt.pageX - window.innerWidth / 2;
-    const pageY = evt.pageY - window.innerHeight / 2;
-    const newX = width * (pageX * -1);
-    const newY = height * (pageY * -1);
-
-    element.style.backgroundPositionX = newX + "px";
-    element.style.backgroundPositionY = newY + "px";
-}
-
-defineExpose({ movetarget });
-</script>
+<script setup lang="ts"></script>
 
 <template>
-    <div>
-        <!-- OverlayedGradient -->
-        <div
-            v-on:mousemove="onMove"
-            class="w-screen h-screen bg-top fixed -z-10 bg-gradient-to-t from-gray-600 to-gray-300 mix-blend-multiply border-8 border-gray-800"
-        />
-        <!-- BG Image -->
-        <div
-            ref="movetarget"
-            class="w-screen h-screen bg-cover bg-left-top bg-no-repeat bg-gray-900 bg-[url('/src/assets/background-scroller.jpg')] fixed bg-blue-800 -z-20 filter blur-sm"
-        />
-    </div>
+  <div>
+    <!-- OverlayedGradient -->
+    <div
+      class="w-full h-full -z-10 absolute bg-gradient-to-r from-slate-700 via-transparent to-transparent"
+    />
+    <!-- BG Image -->
+    <div
+      class="w-full h-full -z-20 absolute bg-scroll bg-cover bg-center bg-no-repeat bg-[url('/src/assets/background-scroller.jpg')] bg-slate-800"
+    />
+  </div>
 </template>
