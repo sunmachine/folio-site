50493956f910035e740a5b4ad3cf50c432799e43
Dan Peavey <danpeavey@gmail.com>
2022-02-26 18:45:07 -0800

    Background setup.

--- personal-site/src/App.vue
+++ personal-site/src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import HeaderContainer from "./components/FrontPage/HeaderContainer.vue";
-
+import FixedBackgroundContainer from "./components/FrontPage/FixedBackgroundContainer.vue";
 </script>
 
 <template>
   <div>
+    <!-- Left-side -->
     <div
       class="left-0 w-2/5 h-screen bg-top fixed bg-slate-700 border-l-8 border-t-8 border-b-8 border-gray-800"
     >
@@ -14,17 +15,13 @@ import HeaderContainer from "./components/FrontPage/HeaderContainer.vue";
         </div>
       </div>
     </div>
+
+    <!-- Right-side -->
     <div
-      class="right-0 p-1/2 w-3/5 h-screen absolute bg-cover bg-top bg-no-repeat bg-[url('/src/assets/background-scroller.jpg')] bg-slate-800 border-r-8 border-t-8 border-b-8 border-slate-800"
+      class="right-0 p-2/5 w-3/5 h-screen absolute border-r-8 border-t-8 border-b-8 border-slate-800 bg-clear"
     >
+      <FixedBackgroundContainer />
       <!-- <div class="md:flex-initial w-max md:w-3/5 md:pt-16 pt-4 pl-6"> -->
-      <p class="text-slate-300">New site updates coming soon. I'm serious.</p>
-      <div class="text-slate-700 absolute bottom-0 right-0">
-        <svg class="fill-current" viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg">
-          <polygon points="0 25, 0 50, 100 50, 100 0" />
-        </svg>
-      </div>
-      <!-- </div> -->
     </div>
   </div>
 </template>
